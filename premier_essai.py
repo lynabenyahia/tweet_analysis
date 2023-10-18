c489553030213b47cb2596c424d38817a262a1a1
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-10-18 15:23:44 +0200

    Update premier_essai.py

--- premier_essai.py
+++ premier_essai.py
@@ -11,6 +11,12 @@ import re
 import nltk.corpus 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import warnings
+
+warnings.filterwarnings("ignore")
 
 # Importing the dataframe
 df = pd.read_csv('clean_data_tweet_sample.csv')  
@@ -87,3 +93,270 @@ df = df.loc[:, selected_columns]
 
 
 
+# =============================================================================
+# Data visualisation
+# =============================================================================
+
+# Create a new column with the date in format: YYYY-MM
+df['date_M'] = df['created_at'].apply(lambda x: x[:7])
+
+fig = px.line(df.groupby('date_M').count(), 
+              y=df.groupby('date_M').count()['author.name'].values, 
+              x=df.groupby('date_M').count().index)
+fig.update_layout(plot_bgcolor = "white")
+fig.update_layout(
+    title="Number of Tweets per months",
+    xaxis_title="",
+    yaxis_title="Number of Tweets",
+    font=dict(
+         size=12,
+        color="Black"
+    ),
+    plot_bgcolor = "white"
+)
+fig.show()
+
+# Plot of the number of tweets per month
+fig = make_subplots(rows=2, cols=3)
+
+fig.add_trace(
+    go.Line(x=df.loc[df['label'] == 'en'].groupby('date_M').count().index, 
+            y=df.loc[df['label'] == 'en'].groupby('date_M').count()['author.name'].values,
+            name='England'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Line(x=df.loc[df['label'] == 'es'].groupby('date_M').count().index, 
+            y=df.loc[df['label'] == 'es'].groupby('date_M').count()['author.name'].values,
+            name='Spain'),
+    row=1, col=2
+)
+
+fig.add_trace(
+    go.Line(x=df.loc[df['label'] == 'de'].groupby('date_M').count().index, 
+            y=df.loc[df['label'] == 'de'].groupby('date_M').count()['author.name'].values,
+            name='Germany'),
+    row=1, col=3
+)
+
+fig.add_trace(
+    go.Line(x=df.loc[df['label'] == 'nl'].groupby('date_M').count().index, 
+            y=df.loc[df['label'] == 'nl'].groupby('date_M').count()['author.name'].values,
+            name='Netherlands'),
+    row=2, col=1
+)
+
+fig.add_trace(
+    go.Line(x=df.loc[df['label'] == 'fr'].groupby('date_M').count().index, 
+            y=df.loc[df['label'] == 'fr'].groupby('date_M').count()['author.name'].values,
+            name='France'),
+    row=2, col=2
+)
+
+fig.add_trace(
+    go.Line(x=df.loc[df['label'] == 'it'].groupby('date_M').count().index, 
+            y=df.loc[df['label'] == 'it'].groupby('date_M').count()['author.name'].values,
+            name='Italy'),
+    row=2, col=3
+)
+
+fig.update_layout(height=600, width=800, title_text="Number of Tweets per months")
+fig.show()
+
+
+# Number of tweets per author
+fig = px.bar(df.groupby('author.name').count().sort_values(by='id', ascending=False).head(20), 
+              y=df.groupby('author.name').count().sort_values(by='id', ascending=False).head(20)['id'].values, 
+              x=df.groupby('author.name').count().sort_values(by='id', ascending=False).head(20).index)
+fig.update_layout(plot_bgcolor = "white")
+fig.update_layout(
+    title="Number of Tweets per author (top 20)",
+    xaxis_title="",
+    yaxis_title="Number of Tweets",
+    font=dict(
+         size=12,
+        color="Black"
+    ),
+    plot_bgcolor = "white"
+)
+fig.show()
+
+# Top 5 per country
+fig = make_subplots(rows=2, cols=3)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'en'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'en'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5)['id'].values,
+            name='England'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'es'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'es'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5)['id'].values,
+            name='Spain'),
+    row=1, col=2
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'de'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'de'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5)['id'].values,
+            name='Germany'),
+    row=1, col=3
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'nl'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'nl'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5)['id'].values,
+            name='Netherlands'),
+    row=2, col=1
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'fr'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'fr'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5)['id'].values,
+            name='France'),
+    row=2, col=2
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'it'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'it'].groupby('author.name').count().sort_values(by='id', ascending=False).head(5)['id'].values,
+            name='Italy'),
+    row=2, col=3
+)
+
+fig.update_layout(height=600, width=800, title_text="Number of Tweets per author (top 5)")
+fig.show()
+
+
+# Number of likes per author
+fig = px.bar(df.groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(20), 
+              y=df.groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(20).values, 
+              x=df.groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(20).index)
+fig.update_layout(plot_bgcolor = "white")
+fig.update_layout(
+    title="Total number of likes per author (top 20)",
+    xaxis_title="",
+    yaxis_title="Total number of likes",
+    font=dict(
+         size=12,
+        color="Black"
+    ),
+    plot_bgcolor = "white"
+)
+fig.show()
+
+# Top 5 per country
+fig = make_subplots(rows=2, cols=3)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'en'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'en'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).values,
+            name='England'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'es'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'es'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Spain'),
+    row=1, col=2
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'de'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'de'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Germany'),
+    row=1, col=3
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'nl'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'nl'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Netherlands'),
+    row=2, col=1
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'fr'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'fr'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).values,
+            name='France'),
+    row=2, col=2
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'it'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'it'].groupby('author.name')['public_metrics.like_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Italy'),
+    row=2, col=3
+)
+
+fig.update_layout(height=600, width=800, title_text="Total number of likes per author (top 5)")
+fig.show()
+
+# Total retweets per author
+fig = px.bar(df.groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(20), 
+              y=df.groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(20).values, 
+              x=df.groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(20).index)
+fig.update_layout(plot_bgcolor = "white")
+fig.update_layout(
+    title="Total number of retweets per author (top 20)",
+    xaxis_title="",
+    yaxis_title="Total number of retweets",
+    font=dict(
+         size=12,
+        color="Black"
+    ),
+    plot_bgcolor = "white"
+)
+fig.show()
+
+# Top 5 per country
+fig = make_subplots(rows=2, cols=3)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'en'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'en'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).values,
+            name='England'),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'es'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'es'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Spain'),
+    row=1, col=2
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'de'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'de'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Germany'),
+    row=1, col=3
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'nl'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'nl'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Netherlands'),
+    row=2, col=1
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'fr'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'fr'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).values,
+            name='France'),
+    row=2, col=2
+)
+
+fig.add_trace(
+    go.Bar(x=df.loc[df['label'] == 'it'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).index, 
+            y=df.loc[df['label'] == 'it'].groupby('author.name')['public_metrics.retweet_count'].sum().sort_values(ascending=False).head(5).values,
+            name='Italy'),
+    row=2, col=3
+)
+
+fig.update_layout(height=600, width=800, title_text="Total number of retweets per author (top 5)")
+fig.show()
