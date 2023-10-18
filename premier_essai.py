0e0b8198a961c3782ecfce9f08200f122cd9ef27
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-10-18 15:09:23 +0200

    merge Zied et Lyna

--- premier_essai.py
+++ premier_essai.py
@@ -15,6 +15,54 @@ from nltk.corpus import stopwords
 # Importing the dataframe
 df = pd.read_csv('clean_data_tweet_sample.csv')  
   
+
+# =============================================================================
+# Extracting the hashtags and links
+# =============================================================================
+def extract_tags(text:str)->dict(): 
+    d = {}
+    try:
+        tags = re.findall(r'#\w+',text)
+        d['hash_tags'] = tags
+    except: 
+        d['hash_tags'] = []
+    try:
+        d['links'] = re.findall(r'(https://t.co/\w+|http://t.co/\w+)',text)
+        for link in d['links']:
+            text = text.replace(link,'')
+    except:
+        d['links'] = []
+    
+    try:
+        for tag in tags:
+            text = text.replace(tag,'')
+        text = text.strip()
+        d['text'] = text
+    except:
+        d['text'] = text
+    
+    return d
+    
+
+extract_tags(df['text'][3])
+
+def clean_text_col(df):
+    text = list()
+    links = list()
+    tags = list()
+    d_clean = dict()
+    for r in range(0,len(df)):
+        text.append(extract_tags(df['text'][r])['text'])
+        tags.append(extract_tags(df['text'][r])['hash_tags'])
+        links.append(extract_tags(df['text'][r])['links'])
+    
+    df['links'] = links
+    df['hash_tags'] = tags        
+    return df 
+
+df = clean_text_col(df)
+
+
 # =============================================================================
 # Cleaning the data
 # =============================================================================
@@ -34,10 +82,8 @@ df['text'] = df['text'].apply(lambda x: cleaning(x))
 df = df.drop_duplicates('text') # drop the duplicates
 
 # Selecting only the columns that we interested in
-selected_columns = ['id', 'created_at', 'text', 'author.name', 'label', 'public_metrics.like_count', 'public_metrics.retweet_count']
+selected_columns = ['id', 'created_at', 'text', 'author.name', 'label', 'public_metrics.like_count', 'public_metrics.retweet_count', 'links', 'hash_tags']
 df = df.loc[:, selected_columns]
-
-
 
 
 
